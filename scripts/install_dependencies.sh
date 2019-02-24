131e2902e2a608625d6199dad7a20138fb289620
Thomas Geissl <thomas.geissl@gmail.com>
2019-02-24 20:15:23 -0300

    added mqtt library

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,3 +4,9 @@ mkdir libs
 cd libs
 git clone https://github.com/thomasgeissl/QOSC.git
 git clone https://github.com/nlohmann/json.git
+
+git clone git://code.qt.io/qt/qtmqtt.git
+cd qtmqtt
+qmake
+make
+make install
